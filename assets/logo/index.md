4bfcf4fa7e9bbbca8ef7fb846604ca3ac2a487a6
fgarcia <253157+fgarcia@users.noreply.github.com>
2023-12-16 17:16:19 +0100

    wip

---
+++ assets/logo/index.md
@@ -0,0 +1,4 @@
+---
+beta:
+  fetch: ca77b0ab-eb6e-44bb-8398-075ca8008ae8
+---
--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-npx vsce package
+name=$(cat package.json | jq -r .name)
+version=$(cat package.json | jq -r .version)
+file="$name-$version.vsix"
+cmd="npx vsce package --out _build/vsix/$file"
+echo + $cmd
+eval $cmd
